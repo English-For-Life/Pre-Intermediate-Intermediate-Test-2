b66d15794d717d50da1d4714d857d8cd49b3c8b3
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-08 09:12:18 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,104 @@
+//Array of objects
+const quiz = [
+    {
+        q:"So, Mr White, what ....... yesterday at 5 o’clock in the evening?",
+        options:["were you doing", "have you done", "had you done", "did you do"],
+        answer: 0
+    },
+    {
+        q:"There aren’t ....... pens on the desk.",
+        options:["some", "any", "a", "no"],
+        answer: 1
+    },
+    {
+        q:" ....... a cup of coffee? – No, thank you.",
+        options:["Are you like", "Do you like", "Would you like", "Want you"],
+        answer: 2
+    },
+    {
+        q:"It’s fantastic, ....... ?",
+        options:["does it", "is it", "doesn't it", "isn't it" ],
+        answer: 3
+    },
+    {
+        q:"He sometimes ....... for a walk after work.",
+        options:["goes", "go", "is going", "going"],
+        answer: 0
+    },
+    {
+        q:"What ....... Betty ....... now?",
+        options:["..... doing", "is ..... doing", "does ..... do",  "do ..... do"],
+        answer: 1
+    },
+    {
+        q:"Your car is very old. – Yes, we ....... buy a new one soon.",
+        options:["is going to", "will", "are going to", "want"],
+        answer: 2   
+    },
+    {
+        q:"What ....... you learn at school yesterday?",
+        options:["were", "do", "had", "did"],
+        answer: 3
+    },
+    {
+        q:"Where ....... you yesterday?",
+        options:["were", "did", "be", "was"],
+        answer: 0
+    },
+    {
+        q:"Jane sings ....... me.",
+        options:["better as", "as well as", "as good as", "well as"],
+        answer: 1
+    },
+    {
+        q:"He speaks English ....... me.",
+        options:["best than", "so better than", "better than", "as better than"],
+        answer: 2
+    },
+    {
+        q:"This building ....... in 1910.",
+        options:["had built", "built", "is built", "was built"],
+        answer: 3
+    },
+    {
+        q:"I’ve got a headache. I think I ....... go to bed now.",
+        options:["will", "want to", "am going to", "-"],
+        answer: 0
+    },
+    {
+        q:"She ....... in London before she moved to China.",
+        options:["lived", "had lived", "have lived", "was living"],
+        answer: 1
+    },
+    {
+        q:"He ....... met a famous person.",
+        options:["have never", "doesn't never", "has never", "didn't ever"],
+        answer: 2
+    },
+    {
+        q:"That’s it! I ....... my task!",
+        options:["finished", "did finish", "were finishing", "have finished"],
+        answer: 3
+    },
+    {
+        q:"We ....... buy the books, we can borrow them from the library.",
+        options:["don't have to", "can't", "shouldn't", "mustn't"],
+        answer: 0
+    },
+    {
+        q:"....... ever ....... to Spain?",
+        options:["Do you ..... went", "Have you ..... been", "Are you ..... go", "Did you ..... go"],
+        answer: 1
+    },
+    {
+        q:"If she ....... , she’ll get a grant to study in France.",
+        options:["will pass", "passed", "passes", "is going to pass"],
+        answer: 2
+    },
+    {
+        q:"She ....... in Moscow before she moved to London.",
+        options:["was living", "has lived", "lived", "had lived"],
+        answer: 3
+    }
+
+]
